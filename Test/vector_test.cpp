ef91fdcb6828989cbde61d52fb846021fc7fd1dd
天知いのり <65227082+AmachiInori@users.noreply.github.com>
2020-10-19 00:40:50 +0800

    完美转发测试

--- Test/vector_test.cpp
+++ Test/vector_test.cpp
@@ -1,23 +1,17 @@
-# include <vector>
 # include <iostream>
 # include "../Amachi_STL/AMI_allocate"
+# include "../Amachi_STL/AMI_vector"
 
 int main() {
-    std::vector<int, AMI_std::base_allocator<int>> test = {1, 2, 3, 4, 6};
-    for (auto &a : test) {
-        std::cout << a;
+    int i = 0;
+    AMI_std::vector<int> test(5, 1); //必须是引用？
+    for (auto &i : test) {
+        std::cout << i;
     }
-    std::vector<int, AMI_std::base_allocator<int>> test2 = {1, 2};
-    for (auto &a : test2) {
-        std::cout << a;
-    }
-    std::vector<long long int, AMI_std::base_allocator<long long int>> test3 = {1, 2, 1, 2, 3, 4, 6,1, 2, 3, 4, 6,1, 2, 3, 4, 6,1, 2, 3, 4, 6};
-    for (auto &a : test3) {
-        std::cout << a;
-    }
-    std::vector<int, AMI_std::base_allocator<int>> test4 = {1, 2};
-    for (auto &a : test4) {
-        std::cout << a;
+    i = 1;
+    test.push_back(i);
+    for (auto &i : test) {
+        std::cout << i;
     }
     return 0;
 }
