ad58971837285b814c06a505aa6f6ef581bb9efa
天知いのり <65227082+AmachiInori@users.noreply.github.com>
2020-11-29 01:50:12 +0800

    heap algorithm

--- Amachi_STL/AMI_base/AMI_heap.h
+++ Amachi_STL/AMI_base/AMI_heap.h
@@ -6,7 +6,8 @@
 
 __ASTL_NAMESPACE_START
 
-template <class random_iterator, class _cmp = less<typename __iterator_traits<random_iterator>::value_type>>
+template <class random_iterator, 
+          class _cmp = AMI_std::less<typename __iterator_traits<random_iterator>::value_type>>
 inline void push_heap(random_iterator begin, random_iterator end, _cmp) {
     return __push_heap(begin, end, end - begin, *begin, _cmp());
 }
@@ -17,7 +18,8 @@ inline void __push_heap(random_iterator begin, random_iterator end, distance, va
 }
 
 template <class random_iterator, class _cmp, class distance, class value_type> 
-inline void __percolate_up(random_iterator begin, distance insert_loca, value_type insert_value, _cmp) {
+inline void __percolate_up(random_iterator begin, distance insert_loca, 
+                           value_type insert_value, _cmp) {
     auto father_index = [](distance loca) { return (loca - 1) / 2; };
     distance current = insert_loca;
     for (current = insert_loca; current > 0; current = father_index(current)) {
@@ -27,7 +29,8 @@ inline void __percolate_up(random_iterator begin, distance insert_loca, value_ty
     *(begin + current) = insert_value;
 }
 
-template <class random_iterator, class _cmp = less<typename __iterator_traits<random_iterator>::value_type>>
+template <class random_iterator, 
+          class _cmp = AMI_std::less<typename __iterator_traits<random_iterator>::value_type>>
 inline void pop_heap(random_iterator begin, random_iterator end, _cmp) {
     return __pop_heap(begin, end, end - begin, *begin, _cmp());
 }
@@ -38,7 +41,8 @@ inline void __pop_heap(random_iterator begin, random_iterator end, distance, val
 }
 
 template <class random_iterator, class _cmp, class distance, class value_type> 
-inline void __percolate_down(random_iterator begin, distance end_loca, value_type root_value, _cmp) {
+inline void __percolate_down(random_iterator begin, distance end_loca, 
+                             value_type root_value, _cmp) {
     auto left_child = [](distance root) { return (root + 1) * 2 - 1; };
     auto right_child = [](distance root) { return (root + 1) * 2; };
     distance current = 0;
